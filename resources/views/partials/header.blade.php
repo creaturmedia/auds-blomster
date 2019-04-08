43bd2757799cc2225a0214bb0f6ab5ee8b48ab39
Stian Botnan <stian@creatur.no>
2019-04-08 12:48:26 +0200

    Add BEM menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,13 @@
 <header class="banner">
   <div class="container container row row--vertical-align">
-    <a class="brand" href="{{ home_url('/') }}">
+    <a class="brand" href="{{ get_field('link_to_home', 'option') ? get_field('link_to_home', 'option') : home_url('/') }}">
       @if(get_field('logo', 'option'))
         <img src="{{ get_field('logo', 'option') }}"  />
       @endif
     </a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+      {!! bem_menu('primary_navigation', 'menu') !!}
       @endif
     </nav>
   </div>
