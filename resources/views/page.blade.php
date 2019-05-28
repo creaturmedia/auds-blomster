27e39e2f2d6cc0024e5868abae519f1a41e1ec6a
Rhys Sloman-Brown <me@rslobro.com>
2019-05-28 13:46:32 +0200

    create acf for buttons and style
    Signed-off-by: Rhys Sloman-Brown <me@rslobro.com>

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -8,6 +8,15 @@
         @include('partials.content-page')
       </div>
       @include('partials.sidebar-images')
+      @if (get_field('aktivere_knapper') && $buttons = get_field('knapper'))
+      <div class="col--full page-content">
+        <p>
+          @foreach ($buttons as $button)
+            <a class="btn" href="{{ $button['knappside'] }}">{{ $button['knappetikett'] }}</a>
+          @endforeach
+        </p>
+      </div>
+      @endif
     </div>
   @endwhile
 @endsection
