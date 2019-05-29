0a3c9911c15b5134325dd21dc346d56eedeb7a71
Rhys Sloman-Brown <me@rslobro.com>
2019-05-29 13:53:31 +0200

    change image cropping
    Signed-off-by: Rhys Sloman-Brown <me@rslobro.com>

--- app/setup.php
+++ app/setup.php
@@ -52,7 +52,7 @@ add_action('after_setup_theme', function () {
      * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
      */
     add_theme_support('post-thumbnails');
-    add_image_size('slider', 620, 440, true);
+    add_image_size('slider', 620, 620, true);
     add_image_size('kiste', 445, 150, true);
 
     /**
